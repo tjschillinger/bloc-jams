25cb3dc105aab61bcea7ce6015b9db5791a475d8
Tim <tjschillinger92>
2017-01-19 13:31:58 -0600

    Checkpoint 23 Events: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
  
-     var points = document.getElementsByClassName('point');
+ var animatePoints = function(points) {
  
      var revealPoint = function(x) {
          points[x].style.opacity = 1;
@@ -8,8 +8,27 @@ var animatePoints = function() {
          points[x].style.msTransform = "scaleX(1) translateY(0)";
          points[x].style.WebkitTransform = "scaleX(1) translateY(0)";
      };
+     
     for (var i = 0; i < points.length; i++) {
      revealPoint(i);
- }
+    }
+     
  };
  
+window.onload = function() {
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+      
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);   
+    }
+        
+     });
+ }
