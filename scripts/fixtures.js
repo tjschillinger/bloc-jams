910652243134f42c24faa91a94eda651344b5218
Tim <tjschillinger92>
2017-01-31 10:23:29 -0600

    Checkpoint 32 completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+ 
+var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+var albumHendrix = {
+     title: 'Foxy Lady',
+     artist: 'Jimmy Hendrix',
+     label: '60s',
+     year: '1967',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Foxy Lady', duration: '1:01' },
+         { title: 'Kiss the Sky', duration: '5:01' },
+         { title: 'Pockets of Change', duration: '3:21'},
+         { title: 'What Do You Have Now?', duration: '3:14' },
+         { title: 'Try Again', duration: '2:15'}
+     ]
+ };
